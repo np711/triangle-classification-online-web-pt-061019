fccfe133ab751e460e6829dc89ef804daa8023e1
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-08 21:08:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+ def initialize(side_1, side_2, side_3)
+   @triangel_sides = []
+   @triangel_sides << side_1
+   @triangel_sides << side_2
+   @triangel_sides << side_3
+   
 end
+
+def valid? 
+  
